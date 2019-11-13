b158dfb91a65eed38a36ea233ae8ae47b9b5c049
Matthew Flowers <mattflowers4875@gmail.com>
2019-11-12 19:46:08 -0500

    added tests for tile to ensure that it renders with correct value.

--- src/components/tile/Tile.test.js
+++ src/components/tile/Tile.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, unmountComponentAtNode } from 'react-dom';
 import { act } from 'react-dom/test-utils';
 import Tile from './Tile';
+import { join } from 'path';
 
 let container = null;
 
@@ -16,6 +17,14 @@ afterEach(() => {
     container = null;
 });
 
-it('renders with custom or default text', () => {
+it('renders with custom or default value', () => {
+    act(() => {
+        render(<Tile value='10'/>, container);
+    });
+    expect(container.textContent).toBe('10');
 
+    act(() => {
+        render(<Tile />, container);
+    });
+    expect(container.textContent).toBe('0');
 });
