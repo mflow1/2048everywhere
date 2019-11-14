a31d39293b02adfa000961ea2dd2bc02a1588a46
Matthew Flowers <mattflowers4875@gmail.com>
2019-11-13 22:59:08 -0500

    Added restriction to GenerateTileArray for a minimum of 2x2 array.

--- src/engine/GameEngine.test.js
+++ src/engine/GameEngine.test.js
@@ -1,10 +1,16 @@
-import GameEngine, { GenerateTileArray } from './GameEngine'
+import { GenerateTileArray } from './GameEngine'
 
-it('GenerateArray makes a n x n Array', () => {
-    var x = Math.floor(Math.random() * 11);
-    var y = Math.floor(Math.random() * 11);
+it('GenerateArray makes a n x n Array when above 2x2', () => {
+    var x = Math.floor(Math.random() * 9) + 2;
+    var y = Math.floor(Math.random() * 9) + 2;
     var array = GenerateTileArray(x,y);
     expect(array.length).toBe(x);
     expect(array[0].length).toBe(y);
+});
+
+it('GenerateArray throws when passed less and 2x2', () => {
+    expect(() => {
+        GenerateTileArray(1,1);
+    }).toThrow();
 });
 
