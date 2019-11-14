bcd214bdb6bd9b7d92a709f51ade48bd978733b5
Matthew Flowers <mattflowers4875@gmail.com>
2019-11-13 22:41:35 -0500

    added tileValue to useState on Tile

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Tile.css';
 
 function Tile(props) {
-    var currentValue = 0;
+    const [tileValue, setTileValue] = useState(0);
 
     if (props.value) {
-        currentValue = props.value;
+        setTileValue(props.value);
     }
 
-    return <div className='Tile'>{currentValue}</div>
+    return <div className='Tile'>{tileValue}</div>
 }
 
 export default Tile;
